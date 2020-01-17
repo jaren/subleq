745cd4ce138773eeab3770b8000bec65ec67fbf7
Jaren Mendelsohn <jaren.mendelsohn@gmail.com>
2020-01-17 15:02:23 -0800

    Subleq

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # subleq
 Implementation of a OISC Subleq CPU in SpinalHDL
+https://en.wikipedia.org/wiki/One_instruction_set_computer
 
-# https://en.wikipedia.org/wiki/One_instruction_set_computer
+# ISA Spec
+- Base instruction format: `subleq a b c`
+- `Mem[b] = Mem[b] - Mem[a]; if (Mem[b] <= 0) goto c;`
+- a, b, c are each 10 bits, representing the address of a 32-bit number
+- Each instruction is encoded as a 32-bit word:
+-------------------------------------------
+| 31 30 | 29     20 | 19     10 | 9     0 |
+|  N/A  |     c     |     b     |     a   |
+-------------------------------------------
---
+++ src/main/scala/subleq/Subleq.scala
@@ -0,0 +1,61 @@
+package subleq
+
+import spinal.core._
+import spinal.lib._
+
+class Subleq extends Component {
+  val io = new Bundle {
+    val addr = out Bits(32 bits)
+    val writeEnable = out Bool
+    val writeData = out Bits(32 bits)
+    val readData = in Bits(32 bits)
+  }
+
+  /*
+   * 0 - Read instruction
+   * 1 - Read a
+   * 2 - Read b
+   * 3 - Write b and update pc
+   */
+  val state = RegInit(U"00")
+  state := state + 1
+  when (state === 3) {
+    state := 0
+  }
+
+  val instruction = RegNextWhen(io.readData, state === 0)
+  val a = Bits(10 bits)
+  a := instruction(9 downto 0)
+  val b = Bits(10 bits)
+  b := instruction(19 downto 10)
+  val c = Bits(10 bits)
+  c := instruction(29 downto 20)
+
+  val aVal = RegNextWhen(io.readData, state === 1)
+  val bVal = RegNextWhen(io.readData, state === 2)
+
+  io.writeEnable := state === 3
+  io.writeData := (bVal.asSInt - aVal.asSInt).asBits
+
+  val pcVal = UInt(32 bits)
+  val programCounter = RegNextWhen(pcVal, state === 3) init(0)
+  pcVal := programCounter + 1
+  when (bVal.asSInt <= 0) {
+    pcVal := c.resize(32 bits).asUInt
+  }
+
+  io.addr := pcVal.asBits
+  when (state === 1) {
+    io.addr := a.resize(32 bits)
+  }
+  when (state === 2 | state === 3) {
+    io.addr := b.resize(32 bits)
+  }
+
+}
+
+object Main {
+  def main(args: Array[String]) {
+    SpinalVerilog(new Subleq)
+  }
+}
